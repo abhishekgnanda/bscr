01716b7dcb4685f2c7e308aec77ff439d65b30f8
Abhishek Nanda <ananda@zynga.com>
2012-08-31 19:22:30 +0530

    Moved all sub-scripts to a separate folder
    Signed-off-by: Abhishek Nanda <ananda@zynga.com>

--- main.bash
+++ main.bash
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $DIR/general.bash
-source $DIR/git.bash
-source $DIR/ep-engine.bash
+source $DIR/subscripts/general.bash
+source $DIR/subscripts/git.bash
+source $DIR/subscripts/ep-engine.bash
