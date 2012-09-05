60b4ef8ad7b4d942c541cba2129a2065b6db4193
Abhishek Nanda <ananda@zynga.com>
2012-09-05 16:27:44 +0530

    Added ep-engine aliases for stats
    Signed-off-by: Abhishek Nanda <ananda@zynga.com>

--- subscripts/ep-engine.bash
+++ subscripts/ep-engine.bash
@@ -5,3 +5,6 @@ export MEMBASEPREFIX=/opt/membase
 alias c='./configure --prefix=$MEMBASEPREFIX --with-memcached=$MEMBASEPREFIX --enable-tcmalloc --no-create --no-recursion'
 alias m='./config.status; make'
 alias mc='memcached -v -E .libs/ep.so'
+alias ws='watch -n 1 -d "echo stats | nc 0 11211 | grep -E \"mem_used|curr_items|flusher|diskqueue|resident|ep_inconsistent_slave_chk\""'
+alias we='watch -n 1 -d "echo stats eviction | nc 0 11211"'
+alias wm='watch -n 1 -d "echo stats memory | nc 0 11211"'
