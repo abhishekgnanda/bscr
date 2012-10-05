36d5dbecd6318defb1fcce6110c6c90a03ad83cb
Abhishek Nanda <ananda@zynga.com>
2012-10-05 15:43:27 +0530

    Added default cscope generation to vim
    Signed-off-by: Abhishek Nanda <ananda@zynga.com>

--- subscripts/general.bash
+++ subscripts/general.bash
@@ -1,4 +1,5 @@
 alias rm='rm -i'
+alias vi='cscope -bR; vi'
 
 # ssh auto-complete with previous hosts
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
