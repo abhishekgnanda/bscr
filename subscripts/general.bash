ebcdbdcc11c0c74ec32bd0c132c785ed2cba81a2
Abhishek Nanda <ananda@zynga.com>
2012-10-09 12:59:52 +0530

    Changed vi alias to open with vim
    Signed-off-by: Abhishek Nanda <ananda@zynga.com>

--- subscripts/general.bash
+++ subscripts/general.bash
@@ -1,5 +1,5 @@
 alias rm='rm -i'
-alias vi='cscope -bR; vi'
+alias vi='cscope -bR; vim'
 
 # ssh auto-complete with previous hosts
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
