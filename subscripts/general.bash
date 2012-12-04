6576ad757d9350a11b3ff4f8cdf9fb6eda1c604c
Abhishek Nanda <ananda@zynga.com>
2012-12-04 15:59:16 +0530

    Basic level of autocompletion for scp. It parses the directory structure only as a default setting.
    Signed-off-by: Abhishek Nanda <ananda@zynga.com>

--- subscripts/general.bash
+++ subscripts/general.bash
@@ -3,4 +3,4 @@ alias cs='cscope -bR'
 
 # ssh auto-complete with previous hosts
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
-complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" scp
+complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" -o default scp
