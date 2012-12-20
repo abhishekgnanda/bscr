076abc602ffca9dcd02bb14f0b1214b609099c1d
Abhishek Nanda <ananda@zynga.com>
2012-12-20 16:23:07 +0530

    Added jemalloc aliases for ep-engine (je for setting jemalloc and tc for setting tcmalloc)
    Signed-off-by: Abhishek Nanda <ananda@zynga.com>

--- subscripts/ep-engine.bash
+++ subscripts/ep-engine.bash
@@ -1,4 +1,6 @@
 # ep-engine specific
+alias je="export MEMBASELIBS='-ljemalloc -levent'; export MEMBASECONF=''"
+alias tc="export MEMBASELIBS='-ltcmalloc_minimal -levent'; export MEMBASECONF='--enable-tcmalloc'"
 alias a='if [ $(automake --version | grep "GNU automake" | cut -c 25-28) != "1.11" ]; then pushd /usr/local/Library/Formula; `$(brew versions automake | grep "1.11 " | sed "s/1.11 *//g")`; brew uninstall automake; brew install automake; popd; fi; ./config/autorun.sh'
 alias amc='if [ $(automake --version | grep "GNU automake" | cut -c 25-28) != "1.12" ]; then pushd /usr/local/Library/Formula; `$(brew versions automake | grep "1.12 " | sed "s/1.12 *//g")`; brew uninstall automake; brew install automake; popd; fi; ./config/autorun.sh'
 alias avb='./config/autorun.sh'
@@ -6,12 +8,14 @@ alias r='cl; cscope -bR; a'
 alias rmc='cl; cscope -bR; amc'
 alias rvb='cl; cscope -bR; avb'
 export MEMBASEPREFIX=/opt/membase
+export MEMBASELIBS='-ljemalloc -levent'
+export MEMBASECONF=''
 alias c1='export MEMBASEPREFIX=/opt/membase'
 alias c2='export MEMBASEPREFIX=/opt/membase2'
 alias c3='export MEMBASEPREFIX=/opt/membase3'
-alias c='./configure --prefix=$MEMBASEPREFIX --with-memcached=$MEMBASEPREFIX --enable-tcmalloc --no-create --no-recursion'
-alias cmc='./configure --prefix=$MEMBASEPREFIX --enable-isasl --with-libevent=/opt/membase --no-create --no-recursion'
-alias cvb='./configure --prefix=$MEMBASEPREFIX --with-memcached=$MEMBASEPREFIX --enable-tcmalloc --with-isasl --with-sasl=no'
+alias c='export LIBS=$MEMBASELIBS;./configure --prefix=$MEMBASEPREFIX --with-memcached=$MEMBASEPREFIX $MEMBASECONF --no-create --no-recursion'
+alias cmc='export LIBS="";./configure --prefix=$MEMBASEPREFIX --enable-isasl --with-libevent=/opt/membase --no-create --no-recursion'
+alias cvb='export LIBS=$MEMBASELIBS;./configure --prefix=$MEMBASEPREFIX --with-memcached=$MEMBASEPREFIX $MEMBASECONF --with-isasl --with-sasl=no'
 alias m='./config.status; make'
 alias im='./config.status; sed "s|-O3|-g|g" Makefile > M2; mv M2 Makefile; make'
 alias mc='memcached -v -E .libs/ep.so'
