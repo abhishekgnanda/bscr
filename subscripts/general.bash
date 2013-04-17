696e2d25e610ec995af5f54573e4a13e996683b7
Abhishek Nanda <ananda@zynga.com>
2013-04-17 17:28:16 +0530

    Added a string trim script
    Signed-off-by: Abhishek Nanda <ananda@zynga.com>

--- subscripts/general.bash
+++ subscripts/general.bash
@@ -1,5 +1,6 @@
 alias rm='rm -i'
 alias cs='cscope -bR'
+alias strtrim='sed "s/ *$//g" | sed "s/^ *//g"'
 
 # ssh auto-complete with previous hosts
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
