a419d0935e5d638c7cb01b2ec2bcb2a80dd61c61
Abhishek Nanda <ananda@zynga.com>
2013-07-18 12:39:43 +0530

    Alias for color in ls
    Signed-off-by: Abhishek Nanda <ananda@zynga.com>

--- subscripts/general.bash
+++ subscripts/general.bash
@@ -5,3 +5,9 @@ alias strtrim='sed "s/ *$//g" | sed "s/^ *//g"'
 # ssh auto-complete with previous hosts
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" -o default scp
+
+if [ "$(uname)" == "Darwin" ]; then
+    alias ls='ls -G'
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    alias ls='ls --color'
+fi
