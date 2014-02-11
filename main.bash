ec03718ce0d66c30dbc2108b4f4c913513ffa189
Abhishek Nanda <nandaa@amazon.com>
2014-02-11 12:06:13 +0530

    Disabling ep-engine
    Signed-off-by: Abhishek Nanda <nandaa@amazon.com>

--- main.bash
+++ main.bash
@@ -2,4 +2,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $DIR/subscripts/general.bash
 source $DIR/subscripts/git.bash
-source $DIR/subscripts/ep-engine.bash
+# Disabling ep-engine # source $DIR/subscripts/ep-engine.bash
