307ac0ad368671c69810bb6e854b1d4452db53ac
Abhishek Nanda <abhishek@appier.com>
2014-10-15 20:02:01 +0800

    Aliases for python runs
    Signed-off-by: Abhishek Nanda <abhishek@appier.com>

--- subscripts/appier.bash
+++ subscripts/appier.bash
@@ -1,2 +1,4 @@
 # appier specific
 alias activate='. __/bin/activate'
+alias pbidder='python -m app.main -i staging.ini -f /home/abhishek/yamls/ -p 8002'
+alias pusertr='python -m app.main -i production.ini -p 6420'
