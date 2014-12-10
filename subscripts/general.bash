0af50495c80d2dd962e43dfeb0d84b8c88e3df5f
Abhishek Nanda <abhishek@appier.com>
2014-12-10 10:29:25 +0800

    fix

--- subscripts/general.bash
+++ subscripts/general.bash
@@ -2,7 +2,7 @@ alias rm='rm -i'
 alias cs='cscope -bR'
 alias strtrim='sed "s/ *$//g" | sed "s/^ *//g"'
 alias jb='python -c "import sys, json; print json.dumps(json.load(sys.stdin), sort_keys=True, indent=4)"'
-alias jp='python -c "import sys, json; o = open(sys.argv[-1]); j = json.load(o); o.close(); print json.dumps({key: j[key] for key in sys.argv[1:-1] if key in j})"'
+alias jp='python -c "import sys, json; o = open(sys.argv[-1]); j = json.load(o); o.close(); print json.dumps({key: j[key] for key in sys.argv[1:-1] if key in j}, sort_keys=True)"'
 
 # ssh auto-complete with previous hosts
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
