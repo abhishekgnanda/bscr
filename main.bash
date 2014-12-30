d8d194dddd625b39310c79c9d57388b2c442ea9b
Abhishek Nanda <abhishek@appier.com>
2014-12-30 11:27:27 +0800

    Git update commands

--- main.bash
+++ main.bash
@@ -1,5 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+export _BSCR_DIR=$DIR
+export _BSCR_SSP=$START_STRING_PROMPT
+
 source $DIR/subscripts/general.bash
 source $DIR/subscripts/git.bash
 # Disabling ep-engine # source $DIR/subscripts/ep-engine.bash
--- subscripts/general.bash
+++ subscripts/general.bash
@@ -5,6 +5,7 @@ alias jb='python -c "import sys, json; print json.dumps(json.load(sys.stdin), so
 alias jp='python -c "import sys, json; o = open(sys.argv[-1]); j = json.load(o); o.close(); print json.dumps({key: j[key] for key in sys.argv[1:-1] if key in j}, sort_keys=True)"'
 alias ch='git ls-files | xargs md5sum | md5sum'
 alias gcs='git config color.ui true; git config core.editor vim; git config user.name "Abhishek Nanda"'
+alias bu='pushd $_BSCR_DIR; git pull --rebase; popd; START_STRING_PROMPT=$_BSCR_SSP source $_BSCR_DIR/main.bash'
 
 # ssh auto-complete with previous hosts
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
