d944170f4b8f6298d189cd96b380f14c8c5fce2f
Abhishek Nanda <abhishek@appier.com>
2014-12-30 11:23:38 +0800

    Set git config: Name, color, editor

--- subscripts/general.bash
+++ subscripts/general.bash
@@ -4,6 +4,7 @@ alias strtrim='sed "s/ *$//g" | sed "s/^ *//g"'
 alias jb='python -c "import sys, json; print json.dumps(json.load(sys.stdin), sort_keys=True, indent=4)"'
 alias jp='python -c "import sys, json; o = open(sys.argv[-1]); j = json.load(o); o.close(); print json.dumps({key: j[key] for key in sys.argv[1:-1] if key in j}, sort_keys=True)"'
 alias ch='git ls-files | xargs md5sum | md5sum'
+alias gcs='git config color.ui true; git config core.editor vim; git config user.name "Abhishek Nanda"'
 
 # ssh auto-complete with previous hosts
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
