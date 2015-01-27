18587cc0a0e9d524053fea2fa6285c038b2dd412
Abhishek Nanda <abhishek@appier.com>
2015-01-27 15:50:39 +0800

    normalize alias

--- subscripts/appier.bash
+++ subscripts/appier.bash
@@ -17,3 +17,4 @@ complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g 
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" -o default apscp
 
 alias decompile='python -m app.tool.run_compile -i production.ini -d'
+alias normalize="python -c \"import sys, hashlib; x = sys.stdin.read().strip().split(':'); print hashlib.sha1(x[1]).hexdigest() if x[0] in ['gu', 'gi', 'au', 'ii'] else x[1]\""
