42a013ffd71a8443d281c65c81a1480c2dd7b42f
Abhishek Nanda <abhishek@appier.com>
2017-04-07 16:40:17 +0800

    Clean up some old aliases

--- subscripts/appier.bash
+++ subscripts/appier.bash
@@ -1,14 +1,5 @@
 # appier specific
 alias activate='. __/bin/activate'
-alias pbidder='python -m app.main -i staging.ini -f /home/abhishek/yamls/ -p 8002'
-alias pusertr='python -m app.main -i production.ini -p 6420'
-
-alias del165='ssh -i ~/.ssh/appier-user.pem appier-user@delta165.rtb.appier.net'
-alias del163='ssh -i ~/.ssh/appier-user.pem appier-user@delta163.rtb.appier.net'
-alias ut0='ssh -i ~/.ssh/appier-user.pem appier-user@us-freqcap0.rtb.appier.net'
-alias ut1='ssh -i ~/.ssh/appier-user.pem appier-user@us-freqcap1.rtb.appier.net'
-alias lombard21='ssh -i ~/.ssh/appier-user.pem appier-user@lombard21.rtb.appier.net'
-alias testserver='ssh -i ~/.ssh/appier-user.pem appier-user@192.168.91.158'
 
 alias apssh='ssh -i ~/.ssh/appier-user.pem -l appier-user -o "StrictHostKeyChecking no"'
 alias avssh='ssh -i ~/.ssh/appier-vm.pem -l appier-user -o "StrictHostKeyChecking no"'
