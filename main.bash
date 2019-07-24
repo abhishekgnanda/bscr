90142fd052d1fc41a57b0a1b2df1327b86fd37dd
Abhishek Nanda <abhi.nanda@gmail.com>
2019-07-24 17:37:03 +0800

    Bash history

--- main.bash
+++ main.bash
@@ -3,6 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export _BSCR_DIR=$DIR
 export _BSCR_SSP=$START_STRING_PROMPT
 
+export SHELL_SESSION_HISTORY=0
+export HISTSIZE=100000
+export HISTFILESIZE=10000000
+
 source $DIR/subscripts/general.bash
 source $DIR/subscripts/git.bash
 source $DIR/subscripts/appier.bash
