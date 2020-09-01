33f5661b2e1347898f118e15147729b5b1d7e1dd
Abhishek Nanda <abhi.nanda@gmail.com>
2020-09-01 13:36:57 +0800

    t

--- subscripts/wearisma.bash
+++ subscripts/wearisma.bash
@@ -1,4 +1,4 @@
 alias wi='. $_BSCR_DIR/otherscripts/wearisma_init.sh'
 alias wm='make server'
-alias wa='env $(bin/env) bin/apconsole'
+alias wa='env $(bin/env) bin/console'
 alias rspec_es='rm -rf /tmp/es; elasticsearch -E http.port=9250 -E transport.tcp.port=9351 -E path.data=/tmp/es'
