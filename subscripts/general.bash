3f377fccad0b31594ec09116f15d8969a848cd3e
Abhishek Nanda <abhi.nanda@gmail.com>
2021-04-22 03:19:14 +0800

    t

--- subscripts/general.bash
+++ subscripts/general.bash
@@ -12,9 +12,9 @@ alias top='sudo top'
 alias jctl='sudo journalctl -u'
 alias j='jq -C | less -r'
 
-# ssh auto-complete with previous hosts
-complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
-complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" -o default scp
+## ssh auto-complete with previous hosts
+#complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+#complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" -o default scp
 
 if [ "$(uname)" == "Darwin" ]; then
     alias ls='ls -G'
